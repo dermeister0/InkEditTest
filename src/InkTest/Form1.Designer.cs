d6d468577c0dfc275ef3b4f42af07686377352af
Der_Meister <der@heavysoft.ru>
2020-08-29 01:41:09 -0700

    Initial commit

---
+++ src/InkTest/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace InkTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDisposeAndClear = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDemo1 = new System.Windows.Forms.Button();
+            this.btnDemo2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(149, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(15, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 184);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnDisposeAndClear
+            // 
+            this.btnDisposeAndClear.Location = new System.Drawing.Point(275, 12);
+            this.btnDisposeAndClear.Name = "btnDisposeAndClear";
+            this.btnDisposeAndClear.Size = new System.Drawing.Size(120, 23);
+            this.btnDisposeAndClear.TabIndex = 3;
+            this.btnDisposeAndClear.Text = "Dispose && Clear";
+            this.btnDisposeAndClear.UseVisualStyleBackColor = true;
+            this.btnDisposeAndClear.Click += new System.EventHandler(this.btnDisposeAndClear_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(15, 12);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(120, 23);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnDemo1
+            // 
+            this.btnDemo1.Location = new System.Drawing.Point(275, 166);
+            this.btnDemo1.Name = "btnDemo1";
+            this.btnDemo1.Size = new System.Drawing.Size(120, 23);
+            this.btnDemo1.TabIndex = 5;
+            this.btnDemo1.Text = "Demo 1";
+            this.btnDemo1.UseVisualStyleBackColor = true;
+            this.btnDemo1.Click += new System.EventHandler(this.btnDemo1_Click);
+            // 
+            // btnDemo2
+            // 
+            this.btnDemo2.Location = new System.Drawing.Point(275, 202);
+            this.btnDemo2.Name = "btnDemo2";
+            this.btnDemo2.Size = new System.Drawing.Size(120, 23);
+            this.btnDemo2.TabIndex = 6;
+            this.btnDemo2.Text = "Demo 2";
+            this.btnDemo2.UseVisualStyleBackColor = true;
+            this.btnDemo2.Click += new System.EventHandler(this.btnDemo2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 253);
+            this.Controls.Add(this.btnDemo2);
+            this.Controls.Add(this.btnDemo1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnDisposeAndClear);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDisposeAndClear;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDemo1;
+        private System.Windows.Forms.Button btnDemo2;
+    }
+}
+
---
+++ src/InkTest/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Ink;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InkTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    var ie = new InkEdit { Left = x * 50, Top = y * 50, Width = 40, Height = 40 };
+                    panel1.Controls.Add(ie);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private void btnDisposeAndClear_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in panel1.Controls)
+            {
+                control.Dispose();
+            }
+
+            panel1.Controls.Clear();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+
+
+        private void btnDemo1_Click(object sender, EventArgs e)
+        {
+            var startMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+
+            btnCreate_Click(sender, e);
+            Thread.Sleep(1000);
+            btnClear_Click(sender, e);
+
+            var finalMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+            var diff = (finalMemory - startMemory) / 1024 / 1024;
+
+            MessageBox.Show($"+{diff} MiB");
+        }
+
+        private void btnDemo2_Click(object sender, EventArgs e)
+        {
+            var startMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+
+            btnCreate_Click(sender, e);
+            Thread.Sleep(1000);
+            btnDisposeAndClear_Click(sender, e);
+
+            var finalMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+            var diff = (finalMemory - startMemory) / 1024 / 1024;
+
+            MessageBox.Show($"+{diff} MiB");
+        }
+
+
+    }
+}
